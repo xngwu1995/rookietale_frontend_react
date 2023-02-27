2cf05a3864dd9c8d53a92def0a2e41ceba1c8b0c
xngwu1995 <xngwu1995@gmail.com>
2023-02-27 09:56:27 -0600

    01_Login Used antd to added more feature in login page.

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -1,33 +1,49 @@
 import { useState } from 'react';
 import './index.css';
+import { Button, Input, Form } from 'antd';
+import { Dialog } from 'antd-mobile'
 
+const initialValues = {
+  username: 'hahah',
+  password: '12345'
+}
 const Login = () => {
-  const [name, setName] = useState('');
-  const [pwd, setPwd] = useState('');
+  const onFinish = (values) => {
+    console.log({content: JSON.stringify(values)});
+  };
 
-  console.log(name);
-  const clickHandler = () => {
-    alert('Successfully login ' + name + ', ' + pwd);
-  }
-  const onChangeNameHandler = (e) => {
-    setName(e.target.value)
-  }
-  const onChangePasswordHandler = (e) => {
-    setPwd(e.target.value)
-  }
   return (
     <div className="login">
-      <div>
-        username: <input onChange={onChangeNameHandler} />
-      </div>
-      <div>
-        password: <input type="password" onChange={onChangePasswordHandler} />
-      </div>
-      <div>
-        <button onClick={clickHandler}>
-            Login
-        </button>
-      </div>
+      <Form
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
+        style={{ maxWidth: 600 }}
+        name="basic"
+        initialValues={initialValues}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
