72f7a15cae5f2f2dbed4d6e0b6dd94a50284cccf
xngwu1995 <xngwu1995@gmail.com>
2023-12-04 11:01:52 -0600

    feat: 🎸 Updated the chatgpt UI

--- src/containers/Chatgpt/index.js
+++ src/containers/Chatgpt/index.js
@@ -3,9 +3,15 @@ import { askChatGPT } from '@services/chatgpt';
 import style from './index.module.scss';
 
 const Chatgpt = () => {
-  const [inputData, setInputData] = useState({ requirements: '', content: '' });
+  const [inputData, setInputData] = useState({
+    requirements: '',
+    content: '',
+    wordLimit: '',
+    languageSelect: 'chinese',
+  });
   const [response, setResponse] = useState('');
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
 
   const handleChange = (e) => {
     setInputData({ ...inputData, [e.target.name]: e.target.value });
@@ -13,10 +19,15 @@ const Chatgpt = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault(); // Prevent default form submission behavior
-
+    // Check if any field is empty
+    if (!inputData.requirements
+      || !inputData.wordLimit || !inputData.content || !inputData.languageSelect) {
+      setShowWarning(true);
+      return; // Prevent form submission
+    }
     // Check if the form has already been submitted
     if (isSubmitted) return;
-
+    setShowWarning(false);
     setIsSubmitted(true); // Set isSubmitted to true
 
     try {
@@ -35,7 +46,7 @@ const Chatgpt = () => {
       <form onSubmit={handleSubmit}>
         <div className={style.inputGroup}>
           <label htmlFor="requirements">
-            Requirements
+            需求 (Requirements)
             <input
               type="text"
               id="requirements"
@@ -46,8 +57,21 @@ const Chatgpt = () => {
           </label>
         </div>
         <div className={style.inputGroup}>
+          <label htmlFor="words">
+            字数要求 (Word Limits)
+            <input
+              type="number"
+              id="wordLimit"
+              name="wordLimit"
+              value={inputData.wordLimit}
+              onChange={handleChange}
+              min="1" // Optional: Ensures only positive numbers
+            />
+          </label>
+        </div>
+        <div className={style.inputGroup}>
           <label htmlFor="content">
-            Content
+            内容 (content)
             <textarea
               id="content"
               name="content"
@@ -56,8 +80,28 @@ const Chatgpt = () => {
             />
           </label>
         </div>
+        <div className={style.inputGroup}>
+          <label htmlFor="languageSelect">
+            选择语言 (Select Language)
+            <select
+              id="languageSelect"
+              name="languageSelect"
+              value={inputData.languageSelect}
+              onChange={handleChange}
+            >
+              <option value="chinese">中文 (Chinese)</option>
+              <option value="english">英文 (English)</option>
+            </select>
+          </label>
+        </div>
+        {showWarning && (
+          <div className={style.warningMessage}>
+            请填写所有内容
+            (Please fill in all fields.)
+          </div>
+        )}
         <button type="submit" className={style.submitButton} disabled={isSubmitted}>
-          {isSubmitted ? 'Loading...' : 'Submit'}
+          {isSubmitted ? '加载...' : '提交'}
         </button>
       </form>
       {response && (
