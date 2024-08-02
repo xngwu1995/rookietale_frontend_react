0a3caa3cb4637f53cadadc34dcfc38557a7b7944
xngwu1995 <xngwu1995@gmail.com>
2024-08-02 11:40:33 -0500

    feat: 🎸 Comment devServer

--- craco.config.js
+++ craco.config.js
@@ -1,34 +1,37 @@
-const { CracoAliasPlugin } = require('react-app-alias');
+const { CracoAliasPlugin } = require("react-app-alias");
 
 // webpack config
 module.exports = {
-  devServer: {
-    proxy: {
-      '/api': 'http://0.0.0.0:8000',
-    },
-  },
+  // devServer: {
+  //   proxy: {
+  //     '/api': 'http://0.0.0.0:8000',
+  //   },
+  // },
   style: {
     postcss: {
-      mode: 'extends',
+      mode: "extends",
       loaderOptions: {
         postcssOptions: {
-          ident: 'postcss',
+          ident: "postcss",
           plugins: [
-            ['postcss-px-to-viewport-with-include', {
-              unitToConvert: 'px', // my unit
-              viewportWidth: 1669, // width
-              viewportHeight: 969, // height
-              unitPrecision: 6, // precious
-              propList: ['*'], // all
-              viewportUnit: 'vw', // vw
-              fontViewportUnit: 'vw', // vw
-              selectorBlackList: ['ignore'],
-              minPixelValue: 1, // less than 1 no transfer
-              mediaQuery: true, // default false
-              replace: true, // directly replace?
-              exclude: [/node_modules/], // ignore file
-              landscape: false,
-            }],
+            [
+              "postcss-px-to-viewport-with-include",
+              {
+                unitToConvert: "px", // my unit
+                viewportWidth: 1669, // width
+                viewportHeight: 969, // height
+                unitPrecision: 6, // precious
+                propList: ["*"], // all
+                viewportUnit: "vw", // vw
+                fontViewportUnit: "vw", // vw
+                selectorBlackList: ["ignore"],
+                minPixelValue: 1, // less than 1 no transfer
+                mediaQuery: true, // default false
+                replace: true, // directly replace?
+                exclude: [/node_modules/], // ignore file
+                landscape: false,
+              },
+            ],
           ],
         },
       },
