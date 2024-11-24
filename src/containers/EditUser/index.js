7bb507e451f8d32c2b603bd7b7794b6453ad6f06
xngwu1995 <xngwu1995@gmail.com>
2024-11-24 16:46:36 -0600

    feat: 🎸 Added gender and dob in user regirstration process

--- src/containers/EditUser/index.js
+++ src/containers/EditUser/index.js
@@ -1,20 +1,19 @@
-import { useState } from 'react';
-import { Toast } from 'antd-mobile';
-import { useAppContext } from '@utils/context';
-import { CameraOutline } from 'antd-mobile-icons';
-import { fileByBase64 } from '@utils/';
-import {
-  Button, Input, Modal, Form,
-} from 'antd';
-import { editUser } from '@services/register';
-import { useGoTo } from '@utils/hooks';
-import style from './index.module.scss';
+import { useState } from "react";
+import { Toast } from "antd-mobile";
+import { useAppContext } from "@utils/context";
+import { CameraOutline } from "antd-mobile-icons";
+import { fileByBase64 } from "@utils/";
+import { Button, Input, Modal, Form, DatePicker, Radio } from "antd";
+import { editUser } from "@services/register";
+import { useGoTo } from "@utils/hooks";
+import style from "./index.module.scss";
+import moment from "moment";
 
 /**
-*
-*/
+ *
+ */
 const EditUser = () => {
-  const [avatar, setAvatar] = useState('');
+  const [avatar, setAvatar] = useState("");
   const [file, setFile] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(true);
 
@@ -22,12 +21,12 @@ const EditUser = () => {
   const go = useGoTo();
   const [form] = Form.useForm();
 
-  const onFileChange = (e) => {
+  const onFileChange = e => {
     const { files } = e.target;
     const fls = Object.values(files);
     const fl = fls[0];
     setFile(fl);
-    fileByBase64(fl).then((res) => {
+    fileByBase64(fl).then(res => {
       setAvatar(res);
     });
   };
@@ -38,26 +37,37 @@ const EditUser = () => {
   };
 
   const handleSave = async () => {
-    const values = await form.validateFields();
-    const params = {};
-    if (values) {
-      const newNickname = values.nickname;
-      params.nickname = newNickname;
-    }
-    if (file) {
-      params.avatar = file;
-    }
-    if (params) {
-      const res = await editUser(store.user.id, { ...params });
-      if (res.nickname || res.avatar) {
-        Toast.show('保存成功');
-        const newUser = { ...store.user, ...res };
-        setStore({ user: newUser });
-        setIsModalOpen(false);
-        go('tweets');
+    try {
+      const values = await form.validateFields();
+      const params = {};
+
+      if (values.nickname) {
+        params.nickname = values.nickname;
       }
-    } else {
-      Toast.show('更新编辑全名或者重新上传用户头像');
+      if (values.dob) {
+        params.dob = moment(values.dob).format("YYYY-MM-DD");
+      }
+      if (values.gender) {
+        params.gender = values.gender;
+      }
+      if (file) {
+        params.avatar = file;
+      }
+
+      if (Object.keys(params).length > 0) {
+        const res = await editUser(store.user.id, { ...params });
+        if (res.nickname || res.avatar || res.dob || res.gender) {
+          Toast.show("保存成功");
+          const newUser = { ...store.user, ...res };
+          setStore({ user: newUser });
+          setIsModalOpen(false);
+          go("tweets");
+        }
+      } else {
+        Toast.show("请更新任何信息来保存修改");
+      }
+    } catch (error) {
+      Toast.show("请检查输入是否正确");
     }
   };
 
@@ -80,17 +90,48 @@ const EditUser = () => {
           <div className={style.photoIcon}>
             <CameraOutline />
           </div>
-          <input type="file" className={style.upFile} onChange={onFileChange} accept="image/png.image/jpg" />
-          <img className={style.avatar} src={avatar || store.user.avatar} alt="" />
+          <input
+            type="file"
+            className={style.upFile}
+            onChange={onFileChange}
+            accept="image/png.image/jpg"
+          />
+          <img
+            className={style.avatar}
+            src={avatar || store.user.avatar}
+            alt=""
+          />
         </div>
         <div className={style.content}>
-          <Form
-            form={form}
-          >
+          <Form form={form}>
+            <Form.Item name="nickname">
+              <Input
+                label="Nickname"
+                placeholder="Nickname"
+                value={store.nickname}
+              />
+            </Form.Item>
             <Form.Item
-              name="nickname"
+              name="dob"
+              label="Date of Birth"
+              rules={[
+                { required: true, message: "Please select your date of birth" },
+              ]}
             >
-              <Input label="Nickname" placeholder="Nickname" value={store.nickname} />
+              <DatePicker
+                format="YYYY/MM/DD"
+                placeholder="Select Date of Birth"
+              />
+            </Form.Item>
+            <Form.Item
+              name="gender"
+              label="Gender"
+              rules={[{ required: true, message: "Please select your gender" }]}
+            >
+              <Radio.Group>
+                <Radio value="male">Male</Radio>
+                <Radio value="female">Female</Radio>
+              </Radio.Group>
             </Form.Item>
           </Form>
         </div>
--- src/containers/Register/components/OneStep.js
+++ src/containers/Register/components/OneStep.js
@@ -1,28 +1,25 @@
-import { useState } from 'react';
-import {
-  Input, Form, DatePicker,
-} from 'antd';
-import PropTypes from 'prop-types';
-import Footer from './Footer';
-import style from '../index.module.scss';
+import { useState } from "react";
+import { Input, Form, DatePicker, Radio } from "antd";
+import PropTypes from "prop-types";
+import Footer from "./Footer";
+import style from "../index.module.scss";
 
 const ACCOUNT_TYPE = {
-  TEL: 'TEL',
-  EMAIL: 'EMAIL',
+  TEL: "TEL",
+  EMAIL: "EMAIL",
 };
 /**
  * Signup Page
  */
-const OneStep = ({
-  gotoNextStepHandler,
-}) => {
+const OneStep = ({ gotoNextStepHandler }) => {
   const [form] = Form.useForm();
   const [formData] = useState({
-    username: '',
-    email: '',
-    birthday: '',
-    tel: '',
-    inviation: '',
+    username: "",
+    email: "",
+    birthday: "",
+    tel: "",
+    inviation: "",
+    gender: "",
   });
   const [accountType, setAccountType] = useState(ACCOUNT_TYPE.EMAIL);
   const [footerButtonDisabled, setFooterButtonDisabled] = useState(true);
@@ -34,7 +31,7 @@ const OneStep = ({
     }
     setAccountType(ACCOUNT_TYPE.TEL);
   };
-  const dateFormat = 'YYYY/MM/DD';
+  const dateFormat = "YYYY/MM/DD";
   const onClickNextStep = async () => {
     const validate = await form.validateFields();
     validate.birthday = validate.birthday.format(dateFormat);
@@ -69,33 +66,88 @@ const OneStep = ({
           form={form}
           onValuesChange={onValuesChange}
         >
-          <Form.Item name="username" rules={[{ required: true, message: 'Username can not be empty' }]}>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Username can not be empty" }]}
+          >
             <Input placeholder="username" className={style.input} />
           </Form.Item>
           {accountType === ACCOUNT_TYPE.TEL && (
-            <Form.Item name="tel" rules={[{ required: true, message: 'Need a valid phone number', pattern: /^1(3\d|4[5-9]|5[0-35-9]|6[2567]|7[0-8]|8\d|9[0-35-9])\d{8}$/g }]}>
+            <Form.Item
+              name="tel"
+              rules={[
+                {
+                  required: true,
+                  message: "Need a valid phone number",
+                  pattern:
+                    /^1(3\d|4[5-9]|5[0-35-9]|6[2567]|7[0-8]|8\d|9[0-35-9])\d{8}$/g,
+                },
+              ]}
+            >
               <Input placeholder="phone" className={style.input} />
             </Form.Item>
           )}
           {accountType === ACCOUNT_TYPE.EMAIL && (
-            <Form.Item name="email" rules={[{ required: true, message: 'Need a valid email', pattern: /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/g }]}>
+            <Form.Item
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Need a valid email",
+                  pattern:
+                    /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/g,
+                },
+              ]}
+            >
               <Input placeholder="email" className={style.input} />
             </Form.Item>
           )}
-          <div className={style.changeTypeButton} onClick={onAccountTypeChange}>
-            {accountType === ACCOUNT_TYPE.TEL ? 'Use email' : 'Use phone'}
+          <button
+            className={style.changeTypeButton}
+            onClick={onAccountTypeChange}
+          >
+            {accountType === ACCOUNT_TYPE.TEL ? "Use email" : "Use phone"}
+          </button>
+          <div className={style.birthTitle}>Date of birth(Important!!!)</div>
+          <div>
+            It will determine if today is a favorable day for trading based on
+            your date of birth
           </div>
-          <div className={style.birthTitle}>Date of birth</div>
-          <Form.Item name="birthday" rules={[{ required: true, message: 'Birthday can not be empty' }]}>
-            <DatePicker className={style.datePicker} placeholder="Year/Month/Day" format={dateFormat} />
+          <Form.Item
+            name="birthday"
+            rules={[{ required: true, message: "Birthday can not be empty" }]}
+          >
+            <DatePicker
+              className={style.datePicker}
+              placeholder="Year/Month/Day"
+              format={dateFormat}
+            />
           </Form.Item>
-          <div className={style.invTitle}>Inviation Code: Author first name</div>
-          <Form.Item name="inviation" rules={[{ required: true, message: 'Inviation code can not be empty' }]}>
+          <Form.Item
+            name="gender"
+            label="Gender"
+            rules={[{ required: true, message: "Please select your gender" }]}
+          >
+            <Radio.Group>
+              <Radio value="male">Male</Radio>
+              <Radio value="female">Female</Radio>
+            </Radio.Group>
+          </Form.Item>
+          <Form.Item
+            name="inviation"
+            rules={[
+              { required: true, message: "Inviation code can not be empty" },
+            ]}
+          >
             <Input placeholder="Author first name" className={style.input} />
           </Form.Item>
         </Form>
       </div>
-      <Footer onClickNextStep={onClickNextStep} disabled={footerButtonDisabled} label="Next Step" />
+      <Footer
+        onClickNextStep={onClickNextStep}
+        disabled={footerButtonDisabled}
+        label="Next Step"
+      />
     </div>
   );
 };
